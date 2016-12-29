39b1fc640db5a56b4d67c3519731128b3d5a8d91
unknown <ramakrishnan.h@husky.neu.edu>
2016-12-29 09:46:27 +0530

    removing folders

--- Final_Project/Weather data/extractfolders_sample.py
+++
@@ -1,8 +0,0 @@
-import zipfile,fnmatch,os
-#Sample program
-rootPath = r"path"
-pattern = '*.zip'
-for root, dirs, files in os.walk(rootPath):
-    for filename in fnmatch.filter(files, pattern):
-        print(os.path.join(root, filename))
-        zipfile.ZipFile(os.path.join(root, filename)).extractall(os.path.join(root, os.path.splitext(filename)[0]))
